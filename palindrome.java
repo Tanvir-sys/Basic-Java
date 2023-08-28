4f0fe4449fdfa9a9de7b868cb54d1ebb94cc2264
Md. Tanvir Hossain <163hossaintanvir@gmail.com>
2023-08-28 18:04:36 +0600

    practice

--- palindrome.java
+++ palindrome.java
@@ -5,23 +5,30 @@ public class palindrome {
      public palindrome(int n){
         this.num=n;
         int rev=0;
+        int orgnnum=num;
+
         while(num!=0){
             rev=rev*10 + num%10;
             num=num/10;
+            
 
         }
-        System.out.println(rev);
-        if(rev==num){
+        if(orgnnum==rev){
             System.out.println("palindrome Number");
 
         }else {
             System.out.print("Not Palindrome ");
-        }
+    }
+ 
+       
      }
+
+   
+      
     
 
 public static void main(String[] args) {
-    palindrome p =new palindrome(12345);
+    palindrome p =new palindrome(1221);
 
 }
 
