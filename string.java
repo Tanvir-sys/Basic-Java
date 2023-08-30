122f18793342738e022feb2b2c781b81edd635e2
Md. Tanvir Hossain <163hossaintanvir@gmail.com>
2023-08-30 12:29:20 +0600

    Basic Java

--- string.java
+++ string.java
@@ -1,24 +1,43 @@
+import java.util.Scanner;
+
 public class string {
 
     public String str1;
-    public void string(String str){
+    public String  str2;
+     public  String rev="";
+
+    public void string(String str, String str3){
 
         this.str1=str;
-        int x=str.length();
+        this.str2=str3;
+        str=str3;
 
-        System.out.println("length of  string : " +x);
-        for(int i=x;i>=0;i--){
-            System.out.println(str1[i]);
+        int x=str.length();
+      
+        
+
+       System.out.println();
+        for(int i=x-1;i>=0;i--){
+         rev=rev+str.charAt(i);
             
         }
+        System.out.println(rev);
+
 
        
 
 
     }
     public static void main(String[] args) {
-        string s=new string();
-        s.string("gftrhgf");
+        string s =new string ();
+        
+        System.out.println("Enter The word");
+          s.str1 = "asdewe";
+        
+
+        
+        
+        
 
     }
 
